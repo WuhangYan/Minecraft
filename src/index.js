873ec627dba2e3783fdd5bba03e1fbae501920dc
Wuhang <yanwuhang@gmail.com>
2019-09-07 21:52:09 +0800

    add a single button as a single cell

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { Box } from './components/Box';
 
 ReactDOM.render(
-  <App />,
+  <Box />,
   document.getElementById('root')
 );

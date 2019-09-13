dd1b85d86ccafb1526a097963c5851ac30cf011a
Wuhang <yanwuhang@gmail.com>
2019-09-13 20:59:53 +0800

    add left click event

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,8 +2,27 @@ import React, { useState } from 'react';
 import '../assets/styles/style.css';
 
 export function Box(props) {
+  const element = props.clicked ? props.num : '';
+  const handleClick = (e) => {
+    const btnNum = e.button;
+    if(props.clicked) return;
+    if(btnNum===0 && !props.flag) {
+      if(props.num===9) alert('loose')   //will update in next commits
+      else {
+        if(props.num===0) {
+          props.zero(props.coor);
+        }
+        else{
+          props.open(props.coor);
+        }
+      }
+    }
+
+  }
 
   return (
-    <button onClick={()=>console.log(props)} className='box' />
+    <button onMouseUp={(event)=>handleClick(event)} className='box'>
+      {element}
+    </button>
   )
 }
--- src/components/Boxpanel.js
+++ src/components/Boxpanel.js
@@ -2,58 +2,87 @@ import React, { useState } from 'react';
 import { Box } from './Box';
 import '../assets/styles/style.css';
 
-export function Boxpanel() {
-  let r = 9, c = 9;
-  const mines = genarateMines();
-  let col = [];
-  for(let i=0;i<r;i++){
-    let row = [];
-    for(let j=0;j<c;j++){
-      row.push(<Box key={i+'-'+j} num={mines[i][j]} flag={false} clicked={false} />)
+export function Boxpanel(props) {
+  const [openBox, setOpenBox] = useState(new Array());
+  const mines = props.mines;
+  const r = props.total_row, c = props.total_col;
+  console.log(mines);
+
+  /* zeroBox for storing the box having the no mines around and arr for storing box needed to be auto opened
+  before pusing new box to both array, a check for duplicate is required.
+  */
+  const handleZero = (pos) => {
+    let zeroBox = [], arr = [];
+    zeroBox.push(pos);
+    let loop = 0;
+    while(loop < zeroBox.length) {
+      const posArr = zeroBox[loop++].split('-');
+      const row = parseInt(posArr[0]), col = parseInt(posArr[1]);
+      if(row>0 && col>0 && arr.indexOf((row-1) + '-' + (col-1))<0) {
+        arr.push((row-1) + '-' + (col-1));
+        if(mines[row-1][col-1]===0 && zeroBox.indexOf((row-1) + '-' + (col-1))<0) zeroBox.push((row-1) + '-' + (col-1));
+      }
+      if(row>0 && arr.indexOf((row-1) + '-' + col)<0) {
+        arr.push((row-1) + '-' + col);
+        if(mines[row-1][col]===0 && zeroBox.indexOf((row-1) + '-' + col)<0) zeroBox.push((row-1) + '-' + col);
+      }
+      if(row>0 && col<c-1 && arr.indexOf((row-1) + '-' + (col+1))<0) {
+        arr.push((row-1) + '-' + (col+1));
+        if(mines[row-1][col+1]===0 && zeroBox.indexOf((row-1) + '-' + (col+1))<0) zeroBox.push((row-1) + '-' + (col+1));
+      }
+      if(col>0 && arr.indexOf(row + '-' + (col-1))<0) {
+        arr.push(row + '-' + (col-1));
+        if(mines[row][col-1]===0 && zeroBox.indexOf(row + '-' +  (col-1))<0) zeroBox.push(row + '-' +  (col-1));
+      }
+      if(col<c-1 && arr.indexOf(row + '-' + (col+1))<0) {
+        arr.push(row + '-' + (col+1));
+        if(mines[row][col+1]===0 && zeroBox.indexOf(row + '-' + (col+1))<0) zeroBox.push(row + '-' + (col+1));
+      }
+      if(row<r-1 && col>0 && arr.indexOf((row+1) + '-' + (col-1))<0) {
+        arr.push((row+1) + '-' + (col-1));
+        if(mines[row+1][col-1]===0 && zeroBox.indexOf((row+1) + '-' + (col-1))<0) zeroBox.push((row+1) + '-' + (col-1));
+      }
+      if(row<r-1 && arr.indexOf((row+1) + '-' + col)<0) {
+        arr.push((row+1) + '-' + col);
+        if(mines[row+1][col]===0 && zeroBox.indexOf((row+1) + '-' + col)<0) zeroBox.push((row+1) + '-' + col);
+      }
+      if(row<r-1 && col<c-1 && arr.indexOf((row+1) + '-' + (col+1))<0) {
+        arr.push((row+1) + '-' + (col+1));
+        if(mines[row+1][col+1]===0 && zeroBox.indexOf((row+1) + '-' + (col+1))<0) zeroBox.push((row+1) + '-' + (col+1));
+      }
     }
-    col.push(
-      <div key={i} >
-        {row}
+    setOpenBox([...openBox, ...arr]);
+  }
+
+  const handleOpen = (pos) => {
+    setOpenBox([...openBox, pos]);
+  }
+
+  let row = [];
+  for(let i=0;i<r;i++){
+    let col = [];
+    for(let j=0;j<c;j++){
+      col.push(
+        <Box
+          key={i+'-'+j}
+          num={mines[i][j]}
+          flag={false}
+          open={handleOpen}
+          zero={handleZero}
+          coor={i+'-'+j}
+          clicked={openBox.indexOf(i+'-'+j)>=0}
+        />
+      )
+    }
+    row.push(
+      <div key={i}>
+        {col}
       </div>
     );
   }
   return (
     <div className='box_panel'>
-      {col}
+      {row}
     </div>
   )
-}
-
-function genarateMines() {
-  const r = 9, c = 9;
-  let result = new Array(r);
-  for(let k=0;k<r;k++) {
-    result[k] = new Array(c);
-  }
-  let round = 0;
-  while(round < 10){
-    let i = Math.floor(Math.random()*r);                       //[0, 1)*rows &向下取整 => [0, 8)
-    let j = Math.floor(Math.random()*c);
-    if(result[i][j]!==9) {
-      result[i][j] = 9;
-      round++;
-    }
-  }
-  for(let m=0;m<r;m++) {
-    for(let n=0;n<c;n++) {
-      if(result[m][n]!=9){
-        let num = 0;
-        if(m>0 && n>0 && result[m-1][n-1]===9) num++;
-        if(m>0 && result[m-1][n]===9) num++;
-        if(m>0 && n<c-1 && result[m-1][n+1]===9) num++;
-        if(n>0 && result[m][n-1]===9) num++;
-        if(n<c-1 && result[m][n+1]===9) num++;
-        if(m<r-1 && n>0 && result[m+1][n-1]===9) num++;
-        if(m<r-1 && result[m+1][n]===9) num++;
-        if(m<r-1 && n<c-1 && result[m+1][n+1]===9) num++;
-        result[m][n]=num;
-      }
-    }
-  }
-  return result;
 }
